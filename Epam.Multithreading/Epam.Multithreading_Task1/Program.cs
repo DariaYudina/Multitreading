e30bfa0422c5913884dfb6fc161e1f964bc69538
Daria Yudina <Daria>
2020-07-06 20:38:18 +0400

    create project

---
+++ Epam.Multithreading/Epam.Multithreading_Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Epam.Multithreading_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
