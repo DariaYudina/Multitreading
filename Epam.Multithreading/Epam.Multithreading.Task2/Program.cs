3e658abd6d7498b65857637855d59287f1726b6f
Daria Yudina <Daria>
2020-07-10 13:47:40 +0400

    add task 1 and 2

---
+++ Epam.Multithreading/Epam.Multithreading.Task2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Epam.Multithreading.Task2
+{
+    class Program
+    {
+        static int[] arr;
+        static void Main(string[] args)
+        {
+            arr = new int[10];
+            Task task1 = new Task(() => {
+                Random random = new Random();
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = random.Next(1, 100);
+                }
+                Console.WriteLine("Array random values:");
+                foreach (var item in arr)
+                {
+                    Console.WriteLine(item);
+                }
+            });
+            task1.Start();
+
+            Task task2 = task1.ContinueWith((Task t) =>
+            {
+                Random random = new Random();
+                var randomNumber = random.Next(1, 100);
+                Console.WriteLine($"Random number:{randomNumber}");
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] *= randomNumber;
+                }
+                Console.WriteLine("Multiplies array with random integer:");
+
+                foreach (var item in arr)
+                {
+                    Console.WriteLine(item);
+                }
+            });
+
+            Task task3 = task2.ContinueWith((Task t) =>
+            {
+                    int temp;
+                    for (int i = 0; i < arr.Length - 1; i++)
+                    {
+                        for (int j = i + 1; j < arr.Length; j++)
+                        {
+                            if (arr[i] > arr[j])
+                            {
+                                temp = arr[i];
+                                arr[i] = arr[j];
+                                arr[j] = temp;
+                            }
+                        }
+                    }
+                Console.WriteLine("Sorting array by ascending:");
+
+                foreach (var item in arr)
+                {
+                    Console.WriteLine(item);
+                }
+            });
+
+            Task task4 = task3.ContinueWith((Task t) =>
+            {
+                int summ = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    summ += arr[i];
+                }
+                Console.WriteLine("Average value: ");
+                Console.WriteLine(summ/arr.Length);
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
--- Epam.Multithreading/Epam.Multithreading_Task1/Program.cs
+++ Epam.Multithreading/Epam.Multithreading_Task1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Epam.Multithreading_Task1
 {
@@ -6,7 +7,23 @@ namespace Epam.Multithreading_Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task[] tasks = new Task[100];
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                tasks[i] = Task.Factory.StartNew(Method);
+            }
+
+            Task.WaitAll(tasks);
+            Console.WriteLine("Ending Main method");
+            Console.ReadLine();
+
+        }
+        static void Method()
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                Console.WriteLine($"Task #{Task.CurrentId} – {i}");
+            }
         }
     }
 }
