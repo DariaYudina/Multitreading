e5b9d4cba05c223731d0e1a6d6828eb934ac5999
Daria Yudina <Daria>
2020-07-15 11:59:33 +0400

    add task 3

---
+++ Epam.Multithreading/Epam.Multithreading.Task3/MatrixExt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Epam.Multithreading.Task3
+{
+    static class MatrixExt
+    {
+        public static int RowsCount(this int[,] matrix)
+        {
+            return matrix.GetUpperBound(0) + 1;
+        }
+
+        public static int ColumnsCount(this int[,] matrix)
+        {
+            return matrix.GetUpperBound(1) + 1;
+        }
+
+        public static int[,] MatrixMultiplication(int[,] matrixA, int[,] matrixB)
+        {
+            if (matrixA.ColumnsCount() != matrixB.RowsCount())
+            {
+                throw new Exception("Multiplication is not possible! The number of columns in the first matrix is ​​not equal to the number of rows in the second matrix.");
+            }
+
+            var matrixC = new int[matrixA.RowsCount(), matrixB.ColumnsCount()];
+
+            Parallel.For(0, matrixA.RowsCount(), i =>
+                Parallel.For(0, matrixB.ColumnsCount(), j =>
+                {
+                    matrixC[i, j] = 0;
+                    Parallel.For(0, matrixA.ColumnsCount(), k =>
+                    {
+                        matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+                    });
+                }));
+
+            return matrixC;
+        }
+    }
+
+}
---
+++ Epam.Multithreading/Epam.Multithreading.Task3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Epam.Multithreading.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа для умножения матриц");
+
+            var a = GetMatrixFromConsole("A");
+            var b = GetMatrixFromConsole("B");
+
+            Console.WriteLine("Матрица A:");
+            PrintMatrix(a);
+
+            Console.WriteLine("Матрица B:");
+            PrintMatrix(b);
+
+            var result = MatrixExt.MatrixMultiplication(a, b);
+            Console.WriteLine("Произведение матриц:");
+            PrintMatrix(result);
+
+            Console.ReadLine();
+        }
+        static int[,] GetMatrixFromConsole(string name)
+        {
+            Console.Write("Количество строк матрицы {0}:    ", name);
+            var n = int.Parse(Console.ReadLine());
+            Console.Write("Количество столбцов матрицы {0}: ", name);
+            var m = int.Parse(Console.ReadLine());
+
+            var matrix = new int[n, m];
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = 0; j < m; j++)
+                {
+                    Console.Write("{0}[{1},{2}] = ", name, i, j);
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            return matrix;
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (var i = 0; i < matrix.RowsCount(); i++)
+            {
+                for (var j = 0; j < matrix.ColumnsCount(); j++)
+                {
+                    Console.Write(matrix[i, j].ToString().PadLeft(4));
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+
+}
