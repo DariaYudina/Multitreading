e3f6bbffe86f4d8b2bda5eb9be5c6ae2b5237c6a
Daria Yudina <Daria>
2020-07-19 23:34:00 +0400

    add task 4

---
+++ Epam.Multithreading/Epam.Multithreading.Task4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Epam.Multithreading.Task4
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            Thread myThread = new Thread(new ParameterizedThreadStart(CreateTread));
+            myThread.Start(number);
+            myThread.Join();
+            Console.ReadLine();
+        }
+        public static void CreateTread(object number)
+        {
+            int n = (int)number;
+            if (n > 0)
+            {
+                Console.WriteLine(n);
+                n--;
+                Thread thread = new Thread(new ParameterizedThreadStart(CreateTread));
+                thread.Start(n);
+                thread.Join();
+                Console.WriteLine("Thread " +Thread.CurrentThread.ManagedThreadId + " finished");
+            }
+        }
+    }
+}
