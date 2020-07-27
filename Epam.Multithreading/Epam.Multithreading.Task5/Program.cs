7f91150d7ffd71105552a62674e2ebaea44506bb
Daria Yudina <Daria>
2020-07-28 00:38:49 +0400

    fix task 6

---
+++ Epam.Multithreading/Epam.Multithreading.Task5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace Epam.Multithreading.Task5
+{
+    class Program
+    {
+        static Semaphore sem = new Semaphore(1, 1);
+
+        static void Main()
+        {
+            int number = 10;
+            ThreadPool.QueueUserWorkItem(JobForAThread, number);
+            Thread.Sleep(3000);
+
+            Console.ReadLine();
+        }
+
+        static void JobForAThread(object number)
+        {
+            sem.WaitOne();
+            int n = (int)number;
+            if (n > 0)
+            {
+                Console.WriteLine(n);
+                n--;
+                Console.WriteLine("выполнение внутри потока из пула, thread Id: " + Thread.CurrentThread.ManagedThreadId);
+                ThreadPool.QueueUserWorkItem(JobForAThread, n);
+                Thread.Sleep(50);
+            }
+            sem.Release();
+        }
+
+    }
+}
---
+++ Epam.Multithreading/Epam.Multithreading.Task6/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Epam.Multithreading.Task6
+{
+    class Program
+    {
+        static List<string> collection = new List<string>();
+        static bool collectionChanged = false;
+        static object locker = new object();
+
+        static void Main(string[] args)
+        {
+
+            Parallel.For(0, 20, WorkWithCollection);
+            Console.Read();
+        }
+
+        public static void WorkWithCollection(int x)
+        {
+            lock (locker)
+            {
+                if (collectionChanged == false)
+                {
+                    collection.Add("element");
+                    Console.WriteLine("element added");
+                    collectionChanged = true;
+                    Thread.Sleep(2000);
+                }
+                else
+                {
+                    foreach (var item in collection)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    collectionChanged = false;
+                    Thread.Sleep(2000);
+                }
+            }
+        }
+
+        public static void DisplayCollection()
+        {
+            if(collectionChanged == true)
+            {
+                foreach (var item in collection)
+                {
+                    Console.WriteLine(item);
+                }
+                collectionChanged = false;
+            }
+        }
+    }
+}
